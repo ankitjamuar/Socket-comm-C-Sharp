fa9ffbb3392765e0264aed2dd5c7e29c6184ca7a
Ankit <contact_ankit611@yahoo.com>
2014-01-20 22:48:49 +0530

    First Update

---
+++ socketIO/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace socketIO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.message = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(462, 200);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(379, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Send!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // message
+            // 
+            this.message.Location = new System.Drawing.Point(12, 240);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(345, 20);
+            this.message.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 292);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Socket";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox message;
+    }
+}
+
---
+++ socketIO/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WebSocket4Net;
+using SocketIOClient;
+
+namespace socketIO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //Make it invisible
+            // this.WindowState = FormWindowState.Minimized;
+            //this.ShowInTaskbar = false;
+            Execute();
+        }
+
+        Client socket;
+        public void Execute()
+        {
+            updateData("Starting Client");
+
+            socket = new Client("http://23.23.209.78:8124/"); // url to nodejs 
+
+            socket.Connect();
+             updateData("socket started");
+
+          
+            //Used for sending on Socket
+            socket.On("client_data", (fn) =>
+            {
+                updateData("on client_data");
+               // socket.Emit("partInfo", "TEST");
+            });
+
+
+            //Used for receiving Socket
+            socket.On("message", (data) =>
+            {
+                updateData("Received" + data.Json.Args[0] );
+                MessageBox.Show(data.Json.Args[0]);
+                //data.RawMessage  data.MessageText  data.Json.ToJsonString() data.Json.Args[0]
+
+            });
+
+            
+        }
+
+        public void updateData(String msg)
+        {
+            richTextBox1.AppendText(msg +'\n');
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String msg = message.Text;
+            socket.Emit("client_data",msg);
+            updateData("Sendding on Btton Click");
+        }
+
+    }
+}
